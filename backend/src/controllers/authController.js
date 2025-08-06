05b6da633c4ad248450df28dddc140a83ad4133c
WilliamPotter1 <kalaitzidisk85@gmail.com>
2025-08-06 04:59:44 -0700

    copy backend

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,29 @@
+const User = require('../models/User');
+
+const register = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ message: 'Username already exists' });
+    }
+
+    const user = new User({ username, password });
+    await user.save();
+    
+    res.status(201).json({
+      message: 'User created successfully',
+      user: {
+        id: user._id,
+        username: user.username
+      }
+    });
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error: error.message });
+  }
+};
+
+module.exports = {
+  register
+}; 
